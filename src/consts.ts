069f9f0df55c3b976a91032abbce21eab7f87588
Nealle Page <nealle.page@gmail.com>
2023-11-11 23:35:28 +0000

    some more tweaks using more standard tailwind classes

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,24 @@
 
 export const SITE_TITLE = "Nealle Page";
 export const SITE_DESCRIPTION = "Nealle Page's personal website";
+
+export const menuItems = {
+  items: [
+    {
+      name: "Home",
+      url: "/",
+    },
+    {
+      name: "About",
+      url: "/about",
+    },
+    {
+      name: "Blog",
+      url: "/blog",
+    },
+    {
+      name: "Contact",
+      url: "/contact",
+    },
+  ],
+};
--- tailwind.config copy.mjs
+++ tailwind.config copy.mjs
@@ -2,6 +2,14 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    colors: {
+      light_bg: "#fff",
+      gray_bg: "##e6e6e6",
+      dark_bg: "#1e1e24",
+      dark_text: "#1d1d1f",
+      light_text: "#86868b",
+    },
+
     extend: {},
   },
   plugins: [],
