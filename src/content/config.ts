f567f336ae0b4c4fc231994738fb586976d8f615
Nealle Page <nealle.page@gmail.com>
2023-11-14 02:54:31 +0000

    fixing the latest blog piece

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,25 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+    heroImageAlt: z.string().optional(),
+    isDraft: z.boolean().default(false),
+    tags: z.array(z.string()).optional(),
+    author: z.string().default("Nealle Page"),
+  }),
 });
 
-export const collections = { blog };
+const authors = defineCollection({
+  schema: z.object({
+    name: z.string(),
+  }),
+});
+
+export const collections = { blog, authors };
